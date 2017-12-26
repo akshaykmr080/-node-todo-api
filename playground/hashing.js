791065fcb1a773ec8afcb5634de50172beda8d9c
Akshay Kumar Shreekanth <shreekan@usc.edu>
2017-12-26 20:32:30 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,16 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+// var data = {
+//     id: 4
+// }
 
-var data = {
-    id: 4
-}
-
-var token = jwt.sign(data, 'abc123');
+// var token = jwt.sign(data, 'abc123');
 
 
-var decoded = jwt.verify(token, 'abc123');
+// var decoded = jwt.verify(token, 'abc123');
 
-console.log('decoded data: '+JSON.stringify(decoded));
+// console.log('decoded data: '+JSON.stringify(decoded));
 // var token = {
 //     data,
 //     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
@@ -29,3 +29,15 @@ console.log('decoded data: '+JSON.stringify(decoded));
 // } else {
 //     console.log('data has been modified')
 // }
+
+var pass = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(pass, salt, (err, hash) => {
+//         console.log(hash)
+//     })
+// })
+
+bcrypt.compare(pass, '$2a$10$rQb1qdBlezL0Y8BZGPNfZuVpmfip7G4w9YDbyAYYXpQf6MQA5mgr2', (err, res) => {
+    console.log(res);
+})
