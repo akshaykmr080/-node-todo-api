f8a550b59c7edaac0d9332a5b893b3231435e310
Akshay Kumar Shreekanth <shreekan@usc.edu>
2017-12-27 08:37:22 +0530

    add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -156,7 +156,16 @@ app.post('/users/login', (req,res) => {
     }).catch((err) => {
         res.status(400).send();
     })
+});
+
+app.delete('/users/me/token', authenticate, (req,res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send()
+    }, (err) => {
+        res.status(400).send();
+    })
 })
+
 
 app.listen(port, () => {
     console.log('server started on port '+port);
