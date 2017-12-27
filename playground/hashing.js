4f24f12e55cd1205da418f979c1eab0017ea9cbc
Akshay Kumar Shreekanth <shreekan@usc.edu>
2017-12-27 06:34:41 +0530

    add tests for users route

--- playground/hashing.js
+++ playground/hashing.js
@@ -32,11 +32,11 @@ const bcrypt = require('bcryptjs');
 
 var pass = '123abc!';
 
-// bcrypt.genSalt(10, (err, salt) => {
-//     bcrypt.hash(pass, salt, (err, hash) => {
-//         console.log(hash)
-//     })
-// })
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(pass, salt, (err, hash) => {
+        console.log(hash)
+    })
+})
 
 bcrypt.compare(pass, '$2a$10$rQb1qdBlezL0Y8BZGPNfZuVpmfip7G4w9YDbyAYYXpQf6MQA5mgr2', (err, res) => {
     console.log(res);
---
+++ server/tests/seed/seed.js
@@ -0,0 +1,54 @@
+const {Todo} = require('../../../models/todo'); 
+const {ObjectID} = require('mongodb');
+const {User} = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+const users = [{
+    _id: userOneId,
+    email: 'shreekan@usc.edu',
+    password: 'user1pass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneId, access:'auth'}, 'abc123').toString()
+    }]
+},{
+    _id: userTwoId,
+    email: 'akshay.kmr080@gmail.edu',
+    password: 'user2pass'
+}]
+
+
+const todos = [{
+    _id: new ObjectID(),
+    text: 'First test todo'
+}, {
+    _id: new ObjectID(),
+    text: 'Second test todo',
+    complete: true,
+    completedAt: 333
+}]
+
+const populateTodos = (done) => {
+    Todo.remove({}).then(() => {
+        Todo.insertMany(todos);
+    }).then(() => done())
+}
+
+const populateUsers = (done) => {
+    User.remove({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
+
+        return Promise.all([userOne, userTwo])
+    }).then(() => done());
+}
+
+module.exports = {
+    todos,
+    populateTodos,
+    users,
+    populateUsers
+}
